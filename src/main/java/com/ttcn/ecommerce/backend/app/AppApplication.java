8a99a46446e35a56fc2b47694ed1234d39eacfa3
pnhoa <hoapham1232000@gmail.com>
2021-12-28 20:35:26 +0700

    First commit create project

---
+++ src/main/java/com/ttcn/ecommerce/backend/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.ttcn.ecommerce.backend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttcn/ecommerce/backend/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttcn.ecommerce.backend.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
